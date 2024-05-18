39db71d3732d167a0eb8fda8d60943ce109cce8f
Ninja-5000 <kamatmukulr@outlook.com>
2024-05-19 00:33:14 +0400

    Updated NAVIAC embed format

--- commands/slash/ask.js
+++ commands/slash/ask.js
@@ -31,40 +31,52 @@ module.exports = {
 
       const response = res.data.response;
 
+      console.log(
+        `Request from [${int.user.tag}]:\n{\n    prompt: "${prompt}",\n    response: "${response}"\n}`
+      );
       const embed = new EmbedBuilder()
-        .setTitle("N.A.V.I.A.C.'s response")
-        .setDescription(response)
+        .setTitle("N.A.V.I.A.C's Response")
+        .addFields({ name: "Prompt:", value: `> ${prompt}` })
+        .addFields({ name: "Response:", value: `${response}` })
         .setThumbnail(
           "https://cdn.discordapp.com/avatars/975365560298795008/632ac9e6edf7517fa9378454c8600bdf.png?size=4096"
         )
         .setColor(
-          config.embeds && config.embeds.naviacEmbedColour
-            ? config.embeds.naviacEmbedColour
+          client.config.embeds && client.config.embeds.naviacEmbedColour
+            ? client.config.embeds.naviacEmbedColour
             : "#fecdac"
-        );
+        )
+        .setFooter({
+          iconURL: int.user.displayAvatarURL(), // user pfp
+          text: `Requested by ${int.user.tag} • A mini API interface for NAVIAC`,
+        });
 
       await int.editReply({
         embeds: [embed],
       });
     } catch (e) {
-      if (e.response.status == 429) {
-        int.editReply({
-          content: "`You're being rate-limited. Please try again later.`",
-        });
-      } else if (e.response.status == 500) {
-        int.editReply({
-          content:
-            "`The N.A.V.I.A.C. API is currently facing an internal server error [500]`\n`Please try again later.`",
-        });
-      } else {
-        int.editReply({
-          content: "uhh something went wrong...",
-        });
-      }
+      try {
+        if (e.response.status == 429) {
+          int.editReply({
+            content: "`You're being rate-limited. Please try again later.`",
+          });
+        } else if (e.response.status == 500) {
+          int.editReply({
+            content:
+              "`The N.A.V.I.A.C. API is currently facing an internal server error [500]`\n`Please try again later.`",
+          });
+        } else {
+          int.editReply({
+            content: "uhh something went wrong...",
+          });
+        }
 
-      console.log(
-        `\x1b[31mThe N.A.V.I.A.C. API request failed with status ${e.response.status} (${e.response.statusText})\x1b[0m`
-      );
+        console.log(
+          `\x1b[31mThe N.A.V.I.A.C. API request failed with status ${e.response.status} (${e.response.statusText})\x1b[0m`
+        );
+      } catch (e) {
+        console.log(`\x1b[31mError running N.A.V.I.A.C. request: ${e}\x1b[0m`);
+      }
     }
   },
 };
