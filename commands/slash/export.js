1958c482e4b5cd73d0270ddce0a6ae2cf0311002
Carmelo Canavan <74259203+Ninja-5000@users.noreply.github.com>
2024-07-13 12:51:22 +0400

    made /export command (from stef)

---
+++ commands/slash/export.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const fs = require("fs");
+const { AttachmentBuilder } = require("discord.js");
+
+module.exports = {
+  name: "export",
+  requires: [],
+
+  async execute(client, int) {
+    const inputPath = int.options.getString("path");
+
+    const filePath = path.join(process.cwd(), inputPath);
+
+    if (!fs.existsSync(filePath))
+      return int.reply({
+        content: "This file doesn't exist",
+      });
+
+    const attachment = new AttachmentBuilder()
+      .setFile(filePath)
+      .setName(path.basename(filePath));
+
+    await int.deferReply();
+
+    int.editReply({
+      content: `<https://github.com/Ninja-5000/NinjaHelper/blob/main/${
+        inputPath.startsWith("/") ? inputPath.replace("/", "") : inputPath
+      }>`,
+      files: [attachment],
+    });
+  },
+};
